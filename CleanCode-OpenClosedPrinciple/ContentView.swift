0ac8379f5708d1694260ae626db93332dd2791c0
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-07 15:15:54 +0200

    - Class ObservableObject Inheritance vs Protocol, MVVM Architecture

--- CleanCode-OpenClosedPrinciple/ContentView.swift
+++ CleanCode-OpenClosedPrinciple/ContentView.swift
@@ -15,52 +15,70 @@
 
 
 import SwiftUI
+import Combine
 
-protocol ClockModelProtocol {
+protocol ClockModelProtocol: ObservableObject {
     var hours: String { set get }
     var minutes: String { set get }
     var seconds: String { set get }
     
-    mutating func update()
+    func update()
 }
 
-
-struct ClockModel: ClockModelProtocol {
-    var hours = "00"
-    var minutes = "11"
-    var seconds = "30"
-    
-   mutating func update() {
-       let date = Date()
-       let calendar = Calendar.current
-       
-       let hour = calendar.component(.hour, from: date)
-       let minute = calendar.component(.minute, from: date)
-       let second = calendar.component(.second, from: date)
-       
-       self.hours = String(format: "%02d", hour)
-       self.minutes = String(format: "%02d", minute)
-       self.seconds = String(format: "%02d", second)
+extension ClockModelProtocol {
+    func update() {
+        let date = Date()
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        let second = calendar.component(.second, from: date)
+        
+        self.hours = String(format: "%02d", hour)
+        self.minutes = String(format: "%02d", minute)
+        self.seconds = String(format: "%02d", second)
     }
 }
 
-struct ClockModelTwo: ClockModelProtocol {
-    var hours = "00"
-    var minutes = "11"
-    var seconds = "30"
+class ClockVM: ClockModelProtocol {
+    @Published var hours = "00"
+    @Published var minutes = "00"
+    @Published var seconds = "00"
     
-   mutating func update() {
-       hours = "12"
-       minutes = "22"
-       seconds = "00"
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+            .sink { _ in
+                self.update()
+            }
+            .store(in: &cancellables)
+    }
+}
+
+class ClockTwoVM: ClockModelProtocol {
+    @Published var hours = "00"
+    @Published var minutes = "00"
+    @Published var seconds = "00"
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+            .sink { _ in
+                self.update()
+            }
+            .store(in: &cancellables)
+    }
+    
+    func update() {
+        hours = "24"
     }
 }
 
 
-struct ClockView: View {
-    @State var vm : ClockModelProtocol
-    
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+struct ClockView<T: ClockModelProtocol>: View {
+    @ObservedObject var vm: T
     
     var body: some View {
         HStack {
@@ -71,17 +89,15 @@ struct ClockView: View {
             Text(vm.seconds)
         }
         .font(.largeTitle).monospacedDigit()
-        .onReceive(timer) { _ in
-            vm.update()
-        }
     }
 }
 
-
 struct ContentView: View {
+    @StateObject var vm = ClockTwoVM()
+    
     var body: some View {
         VStack {
-            ClockView(vm: ClockModel())
+            ClockView(vm: vm)
         }
         .padding()
     }
@@ -92,3 +108,34 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//struct ClockModel: ClockModelProtocol {
+//    var hours = "00"
+//    var minutes = "11"
+//    var seconds = "30"
+//
+//   mutating func update() {
+//       let date = Date()
+//       let calendar = Calendar.current
+//
+//       let hour = calendar.component(.hour, from: date)
+//       let minute = calendar.component(.minute, from: date)
+//       let second = calendar.component(.second, from: date)
+//
+//       self.hours = String(format: "%02d", hour)
+//       self.minutes = String(format: "%02d", minute)
+//       self.seconds = String(format: "%02d", second)
+//    }
+//}
+//
+//struct ClockModelTwo: ClockModelProtocol {
+//    var hours = "00"
+//    var minutes = "11"
+//    var seconds = "30"
+//
+//   mutating func update() {
+//       hours = "12"
+//       minutes = "22"
+//       seconds = "00"
+//    }
+//}
