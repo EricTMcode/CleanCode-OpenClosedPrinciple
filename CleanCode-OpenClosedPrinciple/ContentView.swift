97ee9dbcb4e56f75ff4bc3a2188ebfac356f8912
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-07 14:38:11 +0200

    - Initial project

--- CleanCode-OpenClosedPrinciple/ContentView.swift
+++ CleanCode-OpenClosedPrinciple/ContentView.swift
@@ -3,17 +3,62 @@
 //  CleanCode-OpenClosedPrinciple
 //
 //  Created by Eric on 07/07/2023.
-//
+
+/*
+ Open Closed Principle
+ "Software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification."
+ 
+ Dependency Injection
+ 1. Struct Protocol (Model View Architecture)
+ 2. Class ObservableObject Inheritance vs Protocol, MVVM Architecture
+ */
+
 
 import SwiftUI
+
+struct ClockModel {
+    var hours = "00"
+    var minutes = "11"
+    var seconds = "30"
+    
+   mutating func update() {
+       let date = Date()
+       let calendar = Calendar.current
+       
+       let hour = calendar.component(.hour, from: date)
+       let minute = calendar.component(.minute, from: date)
+       let second = calendar.component(.second, from: date)
+       
+       self.hours = String(format: "%02d", hour)
+       self.minutes = String(format: "%02d", minute)
+       self.seconds = String(format: "%02d", second)
+    }
+}
+
+struct ClockView: View {
+    @State var vm = ClockModel()
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        HStack {
+            Text(vm.hours)
+            Text(":")
+            Text(vm.minutes)
+            Text(":")
+            Text(vm.seconds)
+        }
+        .font(.largeTitle).monospacedDigit()
+        .onReceive(timer) { _ in
+            vm.update()
+        }
+    }
+}
+
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ClockView()
         }
         .padding()
     }
