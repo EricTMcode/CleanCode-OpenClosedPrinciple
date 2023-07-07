23f08a23ede715f8a62d3a5b12a08062eadf9736
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-07 14:23:55 +0200

    Initial Commit

---
+++ CleanCode-OpenClosedPrinciple/CleanCode_OpenClosedPrincipleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCode_OpenClosedPrincipleApp.swift
+//  CleanCode-OpenClosedPrinciple
+//
+//  Created by Eric on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanCode_OpenClosedPrincipleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
